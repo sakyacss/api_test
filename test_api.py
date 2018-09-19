2f3871fa986d8d5c6eb3f0784858ba5e86f460aa
sakyacss <41986886+sakyacss@users.noreply.github.com>
2018-09-19 16:39:13 +0530

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -2,11 +2,6 @@ from __future__ import print_function
 import flask
 from flask import request, jsonify
 import re, math
-#from collections import Counter
-#from google.cloud import bigquery
-#from google.oauth2 import service_account
-import pandas as pd
-#from pandas.io import gbq
 import json
 import os
 
@@ -16,7 +11,7 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>API</h1><p>Test Api</p>"
+	return "<h1>API</h1><p>Test Api</p>"
 
 @app.route('/api', methods=['GET'])
 def addjd():
