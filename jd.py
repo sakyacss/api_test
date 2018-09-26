902c66915b4189b9363a10dedfc6ac9007fa7a66
sakyacss <41986886+sakyacss@users.noreply.github.com>
2018-09-26 14:08:05 +0530

    Update jd.py

--- jd.py
+++ jd.py
@@ -3,6 +3,7 @@ import flask
 from flask import request, jsonify
 import re, math
 from flask_cors import CORS
+from random import randint
 #from collections import Counter
 #from google.cloud import bigquery
 #from google.oauth2 import service_account
@@ -19,7 +20,7 @@ app.config["DEBUG"] = True
 def home():
     return "<h1>API</h1><p>Test Api</p>"
 
-
+# JD Dashboard
 @app.route('/addjd', methods=['GET'])
 def addjd():
                
@@ -68,9 +69,70 @@ def addjd():
 
     
     return jsonify(data = a)
+
+# ADD New JD
+@app.route('/addjobdesc', methods=['GET'])
+def addjobdesc():
+    a = eval(request.args['questions'])
+    JD_Name = a['JD_Name']
+    BU_Name = a['BU_Name']
+    L0_interview  = a['L0_interview']
+    L1_Panelist_1 = a['L1_Panelist_1']
+    L1_Panelist_2 = a['L1_Panelist_2']
+    L2_Panelist_1 = a['L2_Panelist_1']
+    L1_Panelist_2 = a['L2_Panelist_2']
+    Designation = a['Designation']
+    Years_of_Exp = a['Years_of_Exp']
+    No_of_Position = a['No_of_Position']
+    Skills = a['Skills']
+    Program_code = a['Program_code']
+    op = randint(1005,1100)
+    return jsonify(data = op)
+
+# View Profile
+@app.route('/viewjd', methods=['GET'])
+def viewjd():
+    a = eval(request.args['questions'])
+               
+    b= [{
+"ID":1001,
+"Name":"Sakya Maiti",
+"Phone No": 1234567890,
+"Profile":"www.google.com",
+"Exp":8,
+"Int Stage":"L0",
+},
+{
+"ID":1001,
+"Name":"Varun Anant",
+"Phone No": 1234567890,
+"Profile":"www.google.com",
+"Exp":5,
+"Int Stage":"L1",
+},
+
+{
+"ID":1002,
+"Name":"Abhishek",
+"Phone No": 1234567890,
+"Profile":"www.google.com",
+"Exp":2,
+"Int Stage":"Selected",
+},
+{
+"ID":1002,
+"Name":"Kamal",
+"Phone No": 1234567890,
+"Profile":"www.google.com",
+"Exp":2,
+"Int Stage":"Complete",
+}]
+    return jsonify(data = b)
+	
 if __name__ == '__main__':
 	import logging
 	logging.basicConfig(filename='error.log',level=logging.DEBUG)
 	port = int(os.getenv('PORT', 5000))
 	print("Starting app on port %d" % port)
 	app.run(debug=True, port=port, host='0.0.0.0')
+
